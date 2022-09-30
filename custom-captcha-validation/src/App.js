108b8e902d2ddcaf1ad968c28d8f2b31faa3ee38
Ahilya-P <ahilyabai.pawar@yash.com>
2022-09-30 21:27:15 +0530

    custom-captcha-validation

---
+++ custom-captcha-validation/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Captcha from './components/captcha';
+function App() {
+  return (
+    <div className="App">
+      <Captcha />
+    </div>
+  );
+}
+
+export default App;
